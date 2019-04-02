6a5603aabda5e02c40e8f5326ff349448d416870
GLEN CUNNINGHAM <glen@cunninghamconsulting.net>
2019-04-02 02:46:51 -0400

    Updates

---
+++ precache-manifest.90b988d9b874c60a332ab888b86eb874.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18d279e87eb4e0a56da2",
+    "url": "/react-router-gists/static/css/main.11d659be.chunk.css"
+  },
+  {
+    "revision": "18d279e87eb4e0a56da2",
+    "url": "/react-router-gists/static/js/main.362ee932.chunk.js"
+  },
+  {
+    "revision": "d2516edded8ee4b2ea7f",
+    "url": "/react-router-gists/static/js/runtime~main.0fdf411b.js"
+  },
+  {
+    "revision": "1df2560e855bd0eb6efc",
+    "url": "/react-router-gists/static/js/2.48438ffa.chunk.js"
+  },
+  {
+    "revision": "63fc164e9ef3a502552b3029809447bb",
+    "url": "/react-router-gists/index.html"
+  }
+];
