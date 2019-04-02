6d7cd4d62a4ea500c53a9c1a816cef8038a480fd
GLEN CUNNINGHAM <glen@cunninghamconsulting.net>
2019-04-02 02:44:57 -0400

    Release to GitHub

--- README.md
+++ README.md
@@ -1,68 +1,11 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Gist browser from React Router demo
 
-## Available Scripts
+This code was transcribed (and slightly modified) from the [React Router Introduction](https://reacttraining.com/react-router/) at the React Training site.
 
-In the project directory, you can run:
+Besides showing the basics of how to use react-router-dom, this also has some interesting code such as:
 
-### `npm start`
-
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+- **dynamically fetching data in a component (LoadFile) with state to trigger a new render once fetched**
+- inline styles as components in the jsx file (such as Main, Sidebar...).  This keeps the main HTML/JSX code from being cluttered by the inline styles, and thus is easier to read.
+- passing props along so components can serve as wrappers (such as the style components)
+- keeping small child components in the same file (nice for demo code to keep in everything in one file, but not so good for promoting component reuse)
+- URL parameters for routes - In this case the end of the URL is the id of the Gist selected in the sidebar.
--- src/App.js
+++ src/App.js
@@ -1,28 +1,149 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+  state = {
+    gists: null
+  }
+
+  componentDidMount() {
+    fetch('https://api.github.com/gists')
+      .then(result => result.json())
+      .then(gists => this.setState({ gists: gists }));
+  }
+
   render() {
+    const { gists } = this.state;
+    // console.log(gists);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Root>
+          <Sidebar>
+            {gists ? (
+              gists.map(gist => (
+                <SidebarItem key={gist.id}>
+                  {/* Note use of back tick to make a template literal
+                  string that allows an embedded JS variable with ${} */}
+                  <Link to={`/g/${gist.id}`}>
+                    {gist.description || '[no description]'}
+                  </Link>
+                </SidebarItem>
+              ))
+            ) : <div>Loading...</div>}
+          </Sidebar>
+          <Main>
+            <Route exact path='/' render={() => (
+              <h1>Welcome!</h1>
+            )} />
+            {gists &&
+              <Route path='/g/:gistId' render={({ match }) => (
+                <Gist gist={gists.find(g => g.id === match.params.gistId)} />
+              )} />
+            }
+          </Main>
+        </Root>
+      </Router>
     );
   }
 }
+
+const Gist = ({ gist }) => {
+  // Guard against chance that there is no gist, such as when reloading page
+  // and the last selected gist has fallen off the list.
+  if (!gist) return null;
+
+  return (
+    <div>
+      <h1>{gist.description || 'No Description'}</h1>
+      <ul>
+        {Object.keys(gist.files).map(key => (
+          <li key={key}>
+            <b>{key}</b>
+            <LoadFile url={gist.files[key].raw_url}>
+              {(text) => (
+                <pre>{text}</pre>
+              )}
+            </LoadFile>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+const Root = (props) => (
+  <div style={{ display: 'flex' }} {...props} />
+)
+
+const Sidebar = (props) => (
+  <div style={{
+    width: '33vw',
+    height: '100vh',    // keep in viewport to make it scrollable
+    overflow: 'auto',
+    background: '#eee',
+    WebkitOverflowScrolling: 'touch',   // inertia on touchscreens
+  }} {...props} />
+)
+
+const SidebarItem = (props) => (
+  <div style={{
+    whiteSpace: 'nowrap',     // keep on one line
+    textOverflow: 'ellipsis', // end with ... if it overflows
+    overflow: 'hidden',       // prevent horizontal scrollbar
+    padding: '5px 10px',
+  }} {...props} />
+)
+
+const Main = (props) => (
+  <div style={{
+    flex: 1,            // fill remaining space (in flex direction)
+    height: '100vh',    // match viewport height, autoscroll if needed
+    overflow: 'auto',
+  }}>
+    <div style={{ padding: '20px' }} {...props} />
+  </div>
+)
+
+// Loadfile needs to be a full Component to hold state and thus
+// trigger another rendering once the file has been fetched.
+// Initially this will return null, but then React will detect
+// the state change (file state is set) and call render() again.
+class LoadFile extends Component {
+  state = {
+    file: null
+  }
+
+  componentDidMount() {
+    fetch(this.props.url)
+      .then(res => res.text())
+      .then(file => {
+        this.setState({ file })
+      })
+  }
+
+  render() {
+    const { file } = this.state;
+    // console.log(`LoadFile(${this.props.url}): ${file}`)
+    return (
+      file && this.props.children(file)
+    );
+  }
+}
+
+// A simple stateless function like this won't work for LoadFile.
+// There is nothing to return when it is first called, and then
+// no way to trigger a re-rendering of the component once the fetch
+// and text() completes.  This also doesn't return props.children,
+// so there is no easy way to pass the retrieved file to them.
+// The full stateful component above is the way to go.
+//
+// const LoadFile = async (url) => {
+//   console.log(url);
+//   const response = await fetch(url);
+//   const bodyTxt = await response.text();
+//   console.log(bodyTxt);
+//   return bodyTxt;
+// }
 
 export default App;
